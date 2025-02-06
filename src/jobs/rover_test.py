9985e7f94cab3c8a7e9a8709a91eb347b7d65b8c
Loran Friedrich <loran.friedrich@gmail.com>
2025-02-06 15:51:29 -0800

    Adding the start of charge controller

---
+++ src/jobs/rover_test.py
@@ -0,0 +1,56 @@
+# Notes:
+#  ip addr add 10.250.250.2/24 dev end0
+import csv
+from datetime import datetime
+import os
+import time
+
+from database.hardware import load_hardware_from_json_file
+from database.battery_deployment import BatteryDeployment
+from communications.modbus.modbus import modbus_data_acquisition
+from communications.modbus.modbus_map import ModbusMap
+from utils.system_status import collect_system_stats
+
+DATA_PATH = "/root/raptor/data/"
+
+
+def write_to_csv(filename: str, slave_id: int, data_list: dict):
+    """
+    Write Modbus data to CSV with timestamp
+    data_list: List of tuples [(name, value)]
+    """
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    filename = f'{filename}_{slave_id}.csv'
+    file_exists = os.path.exists(filename)
+
+    with open(filename, 'a', newline='') as csvfile:
+        fieldnames = ['Timestamp'] + list(data_list.keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write header if file is new
+        if not file_exists:
+            writer.writeheader()
+
+        # Create row with timestamp and values
+        row_data = {'Timestamp': timestamp}
+        row_data.update({name: value for name, value in data_list.items()})
+        writer.writerow(row_data)
+
+
+if __name__ == "__main__":
+
+    modbus_map = ModbusMap.from_json("../../data/RenogyRover/modbus_map_v1.json")
+    rover = load_hardware_from_json_file("../../data/RenogyRover/charge_controller_deployment.json")
+
+    cnt = 0
+
+    while(cnt < 1000000000):
+        # Get data from each slave
+        values = modbus_data_acquisition(rover, modbus_map, slave_id=1)
+        if values:
+            write_to_csv("rover", 0, values)
+
+        print(f"Data logged at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        print("---------")
+        cnt += 1
+        time.sleep(29)
