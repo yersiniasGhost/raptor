92ce5384dc3a4d7dbf35b6400673f71d7bf4e2b9
Loran Friedrich <loran.friedrich@gmail.com>
2025-02-27 12:35:57 -0800

    UPdating the reconfigure command.

--- src/actions/reconfigure_action.py
+++ src/actions/reconfigure_action.py
@@ -1,19 +1,24 @@
+from typing import Tuple
 from logging import Logger
 from cloud.raptor_configuration import RaptorConfiguration
 from .base_action import Action
 from .action_status import ActionStatus
+from cloud.telemetry_config import TelemetryConfig
+from cloud.mqtt_config import MQTTConfig
+from utils import JSON
 
 
 class ReconfigureAction(Action):
-    async def execute(self, logger: Logger) -> ActionStatus:
+    async def execute(self, telemetry_config: TelemetryConfig,
+                      mqtt_config: MQTTConfig, logger: Logger) -> Tuple[ActionStatus, JSON]:
         logger.info("Starting recommission action")
         try:
             rc = RaptorConfiguration()
             if not rc.get_configuration():
                 logger.error("Unable to recommission Raptor")
-                return ActionStatus.FAILED
+                return ActionStatus.FAILED, {"error": "error"}
             logger.info("Successfully recommissioned Raptor")
-            return ActionStatus.SUCCESS
+            return ActionStatus.SUCCESS, None
         except Exception as e:
             logger.error(f"Error during recommission: {e}")
-            return ActionStatus.FAILED
+            return ActionStatus.FAILED, {"error": str(e)}
