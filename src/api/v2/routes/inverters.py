786614a84dba52f02d4ea0f34bb83c2359d63f19
Loran Friedrich <loran.friedrich@gmail.com>
2025-02-27 17:21:44 -0800

    Fixing name

--- src/api/v2/routes/inverters.py
+++ src/api/v2/routes/inverters.py
@@ -70,7 +70,7 @@ async def get_inverter_data(deployment: Annotated[HardwareDeploymentRoute, Depen
 
         return JSONResponse(content={"data": data, "error": None})
     except Exception as e:
-        logger.error(f"Error getting BMS data: {e}")
+        logger.error(f"Error getting Inverter data: {e}")
         return JSONResponse(content={"data": None, "error": str(e)})
 
 
@@ -78,7 +78,7 @@ async def get_inverter_data(deployment: Annotated[HardwareDeploymentRoute, Depen
 async def get_historical_data(unit_id: int, num_points: int = Query(default=4000, ge=100, le=10000)):
     try:
         # battery = batteries.get_definition(unit_id)
-        filename = f"inverter_{unit_id}.csv"
+        filename = f"inverter2_{unit_id}.csv"
         last_points = deque(maxlen=num_points)
         with open(filename, 'r') as file:
             header = file.readline().strip()
