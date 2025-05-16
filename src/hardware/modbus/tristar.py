90da670bc6f26739aff53e383c01116f33404feb
Loran Friedrich <loran.friedrich@gmail.com>
2025-05-16 11:40:29 -0700

    Added MorningStar Tristar modbus TCP.

---
+++ src/hardware/modbus/tristar.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import List, Dict
+
+from .modbus_hardware import ModbusHardware, ModbusClientType
+
+
+@dataclass
+class Tristar(ModbusHardware):
+
+    def __post_init__(self):
+        self.client_type = ModbusClientType.TCP
+
+    def get_identifier(self, devices: List[dict]) -> Dict[str, str]:
+        output = {d["mac"]: "ID is NA" for d in devices}
+        return output
