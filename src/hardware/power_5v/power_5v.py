b820f8ae2576a35cca5e6e7509924349f8f6cf91
Loran Friedrich <loran.friedrich@gmail.com>
2025-07-07 15:43:04 -0700

    Adding checks to the 5V Power system.

--- src/hardware/power_5v/power_5v.py
+++ src/hardware/power_5v/power_5v.py
@@ -11,19 +11,27 @@ THIS IS TS-7180 ready only!
 class Power5V(metaclass=Singleton):
     def __init__(self):
         self.power_on_requests = 0
-        self.logger = LogManager("power5v.log").get_logger("Power5V")
+        self.logger = LogManager().get_logger("Power5V")
 
     def request_power_on(self):
         self.power_on_requests += 1
+        self.logger.info(f"Request power ON.  Power on requests: {self.power_on_requests}")
+        self.logger.info(self.check_state())
         self._power_on()
+        self.logger.info(self.check_state())
+
 
     def request_power_off(self):
         self.power_on_requests -= 1
+        self.logger.info(f"Request power off.  Remaining requests: {self.power_on_requests}")
+        self.logger.info(self.check_state())
         self._power_off()
+        self.logger.info(self.check_state())
+
+
 
     def _power_on(self):
         """Enable the 5V power supply for DIO outputs on the first time N =1 """
-        self.logger.info(f"Request power ON.  Power on requests: {self.power_on_requests}")
         if self.power_on_requests == 1:
             try:
                 subprocess.run(["gpioset", "5", "16=1"], check=True)
@@ -35,7 +43,6 @@ class Power5V(metaclass=Singleton):
 
 
     def _power_off(self):
-        self.logger.info(f"Request power off.  Remaining requests: {self.power_on_requests}")
         """Disable the 5V power supply"""
         if self.power_on_requests == 0:
             try:
@@ -45,3 +52,29 @@ class Power5V(metaclass=Singleton):
             except subprocess.CalledProcessError as e:
                 self.logger.error(f"Failed to disable 5V power: {e}")
                 return False
+
+    def check_state(self):
+        try:
+            result = subprocess.run(
+                ['gpioget', '5', '16'],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+
+            # gpioget returns '0' or '1' followed by newline
+            gpio_state = result.stdout.strip()
+            if self.power_on_requests > 0:
+                self.logger.info(f"Number of requests: {self.power_on_requests}. State: {gpio_state == '1'}")
+                return gpio_state == '1'
+            else:
+                self.logger.info(f"No power on requests.  State: {gpio_state == '0'}")
+                return gpio_state == '0'
+
+        except subprocess.CalledProcessError as e:
+            self.logger.error("Cannot ready state")
+            raise subprocess.CalledProcessError(
+                e.returncode,
+                e.cmd,
+                f"Failed to read GPIO state: {e.stderr.strip()}"
+            )
