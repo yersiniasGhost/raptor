d1bf7ffa3f0249d11d9f6e0e67eb7b625abd4d12
Loran Friedrich <loran.friedrich@gmail.com>
2025-07-21 10:34:35 -0700

    Added an action specific to database migrator

---
+++ src/actions/database_migrator_action.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+from .base_action import Action
+from .action_status import ActionStatus
+from config.telemetry_config import TelemetryConfig
+from config.mqtt_config import MQTTConfig
+from utils import JSON, LogManager
+from database.database_manager import DatabaseManager
+
+
+class DatabaseMigratorAction(Action):
+
+    async def execute(self, telemetry_config: TelemetryConfig,
+                      mqtt_config: MQTTConfig) -> Tuple[ActionStatus, JSON]:
+        logger = LogManager().get_logger("DatabaseMigratorAction")
+        logger.info(f"Starting Database Migrator: {self.params}")
+        tag = self.params["tag"]
+        try:
+            DatabaseManager().run_schema_sql()
+            logger.info(f"Successfully applied database migration")
+            return ActionStatus.SUCCESS, {"message": f"Migrated database"}
+        except Exception as e:
+            logger.error(f"Error during Database migrator: {e}")
+            return ActionStatus.FAILED, {"error": str(e)}
